cd6e3dd02969c3a84fb26a54d1197a1354ef4100
pcc <pierre@data-fullstack.com>
2023-03-02 12:10:46 +0100

    delete connections-type

--- airflow_provider_bigquery_reservation/__init__.py
+++ airflow_provider_bigquery_reservation/__init__.py
@@ -7,12 +7,6 @@ def get_provider_info():
         "package-name": "airflow-provider-bigquery-reservation",
         "name": "Apache Airflow BigQuery Reservation Provider",
         "description": "Airflow Provider to buy reservation in BigQuery",
-        "connection-types": [
-            {
-                "connection-type": "gcp_bigquery_reservation",
-                "hook-class-name": "airflow_provider_bigquery_reservation.hooks.bigquery_reservation.BigQueryReservationServiceHook",
-            }
-        ],
         "extra-links": [
             "airflow_provider_bigquery_reservation.operators.bigquery_reservation.BigQueryReservationCreateOperator",
             "airflow_provider_bigquery_reservation.operators.bigquery_reservation.BigQueryReservationDeleteOperator",
